328925da8fd31b7d4816cf3d06a7649b007afbb8
João Gabriel Ferreira Ribeiro <151750450+jaumg2004@users.noreply.github.com>
2024-05-30 09:07:22 -0300

    Update Ilhas.java

--- Smartphone/src/main/java/org/example/Smartphone/DragonCity/Ilhas.java
+++ Smartphone/src/main/java/org/example/Smartphone/DragonCity/Ilhas.java
@@ -1,4 +1,41 @@
 package org.example.Smartphone.DragonCity;
 
 public class Ilhas {
+
+    private int idIlhas;
+    private String nomeDaIlha;
+    private int dragonCityIdDragonCity;
+
+    public Ilhas(int idIlhas, String nomeDaIlha, int dragonCityIdDragonCity) {
+        this.idIlhas = idIlhas;
+        this.nomeDaIlha = nomeDaIlha;
+        this.dragonCityIdDragonCity = dragonCityIdDragonCity;
+    }
+
+    // Getters and Setters
+    public int getIdIlhas() {
+        return idIlhas;
+    }
+
+    public void setIdIlhas(int idIlhas) {
+        this.idIlhas = idIlhas;
+    }
+
+    public String getNomeDaIlha() {
+        return nomeDaIlha;
+    }
+
+    public void setNomeDaIlha(String nomeDaIlha) {
+        this.nomeDaIlha = nomeDaIlha;
+    }
+
+    public int getDragonCityIdDragonCity() {
+        return dragonCityIdDragonCity;
+    }
+
+    public void setDragonCityIdDragonCity(int dragonCityIdDragonCity) {
+        this.dragonCityIdDragonCity = dragonCityIdDragonCity;
+    }
+
 }
+
